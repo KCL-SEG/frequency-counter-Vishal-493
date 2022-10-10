9ae19462a9770fa8113636b6085e23eeb32a6823
Vishal-493 <114832602+Vishal-493@users.noreply.github.com>
2022-10-10 12:35:25 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,10 +4,13 @@
 def frequencies(items):
     frequencies = {}
     newList = []
-    for i in items:
-        if str(i) not in newList:
-            newList.append(str(i))
-            frequencyCounter = (items.count(str(i)))
-            frequencies[str(i)] = frequencyCounter
-        else:
-            continue
+    if len(items) != 0
+        for i in items:
+            if str(i) not in newList:
+                newList.append(str(i))
+                frequencyCounter = (items.count(str(i)))
+                frequencies[str(i)] = frequencyCounter
+            else:
+                continue
+    else:
+        print("Incorrect input")
