84b16f363c08bd9f82dc704f9d34c2b0a305411b
Vishal-493 <114832602+Vishal-493@users.noreply.github.com>
2022-10-10 11:49:45 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
-    return frequencies
+    for (i=0; i < len(items); i++):
+        if items[i] not in newList:
+            newList.append(items[i])
+            frequencyCounter = (items[i].count())
+            frequencies[items[i]] = frequencyCounter
+        else:
+            continue
